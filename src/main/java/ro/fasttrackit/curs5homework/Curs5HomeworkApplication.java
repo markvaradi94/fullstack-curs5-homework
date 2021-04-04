9674ed6fc4dfbefaa0320642168cf7c3f0e90f4a
Mark Varadi <mark.varadi03@gmail.com>
2021-04-04 22:19:40 +0300

    code refactor

--- src/main/java/ro/fasttrackit/curs5homework/Curs5HomeworkApplication.java
+++ src/main/java/ro/fasttrackit/curs5homework/Curs5HomeworkApplication.java
@@ -2,8 +2,10 @@ package ro.fasttrackit.curs5homework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Curs5HomeworkApplication {
 
     public static void main(String[] args) {
--- src/main/java/ro/fasttrackit/curs5homework/config/CountryFileConfig.java
+++ src/main/java/ro/fasttrackit/curs5homework/config/CountryFileConfig.java
@@ -1,23 +1,25 @@
 package ro.fasttrackit.curs5homework.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
 
-@Configuration
+@EnableConfigurationProperties(CountryFileConfig.class)
 @ConfigurationProperties(prefix = "country")
+@ConstructorBinding
 @Validated
 public class CountryFileConfig {
     @NotBlank
-    private String sourceFile;
+    private final String sourceFile;
+
+    public CountryFileConfig(@NotBlank String sourceFile) {
+        this.sourceFile = sourceFile;
+    }
 
     public String getSourceFile() {
         return sourceFile;
-    }
-
-    public void setSourceFile(String sourceFile) {
-        this.sourceFile = sourceFile;
     }
 }
